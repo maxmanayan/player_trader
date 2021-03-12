dd8603f5845549b1c87b5872a2d28b4b3daf3d82
Max Manayan <maximillian521@gmail.com>
2021-03-12 15:39:46 -0700

    articles controller setup

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,23 @@
 class Api::ArticlesController < ApplicationController
+
+  def index 
+
+  end
+
+
+  def show
+
+  end
+
+
+  def create 
+
+  end
+
+
+  private 
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
